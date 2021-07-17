01e8fcbba3bebe4846d454459722772dd67abf1f
Deep Patel <dmpate@deakin.edu.au>
2021-07-17 18:08:21 +1000

    Added logic to calculate the tip

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,11 +1,44 @@
 package com.example.tiptime
 
+import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+
+        binding.calculateButton.setOnClickListener { calculateTip() }
+
+    }
+
+//    Function to calculate the tip
+    fun calculateTip(){
+//        Calculate the tip
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDouble()
+        val selectedId = binding.tipOptions.checkedRadioButtonId
+
+//        Depends on the selected Radio button determine the percentage
+        val tipPercentage = when(selectedId){
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+
+//        Calculate the tip
+        var tip = tipPercentage * cost
+
+//        Round up the tip if required
+        val roundUp = binding.roundUpSwitch.isChecked
+        if(roundUp){
+            tip = kotlin.math.ceil(tip)
+        }
     }
 }
