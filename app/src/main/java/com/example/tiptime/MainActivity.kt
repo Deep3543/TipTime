2a8d952d0327704cbff00b8aad3cd43228cea3bb
Deep Patel <dmpate@deakin.edu.au>
2021-07-17 18:19:20 +1000

    Added logic to calculate the tip - Formatted Tip - Added Documentation Comments - Finalized code

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tiptime.databinding.ActivityMainBinding
+import java.text.NumberFormat
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,10 +24,9 @@ class MainActivity : AppCompatActivity() {
 //        Calculate the tip
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDouble()
-        val selectedId = binding.tipOptions.checkedRadioButtonId
 
 //        Depends on the selected Radio button determine the percentage
-        val tipPercentage = when(selectedId){
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
@@ -40,5 +40,11 @@ class MainActivity : AppCompatActivity() {
         if(roundUp){
             tip = kotlin.math.ceil(tip)
         }
+
+//        Format the tip
+        val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
+
+//        Assign the tip result
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
