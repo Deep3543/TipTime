ff320bd129a8734a859ba9f4e14605d7ef88b70d
Deep Patel <dmpate@deakin.edu.au>
2021-07-17 19:57:22 +1000

    Fixed the problem where result was not showing up

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,44 +1,40 @@
 package com.example.tiptime
 
-import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
+import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_main)
-
+        setContentView(binding.root)
         binding.calculateButton.setOnClickListener { calculateTip() }
-
     }
 
 //    Function to calculate the tip
-    fun calculateTip(){
+    private fun calculateTip(){
 //        Calculate the tip
         val stringInTextField = binding.costOfService.text.toString()
         val cost = stringInTextField.toDouble()
 
 //        Depends on the selected Radio button determine the percentage
-        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId){
+    val tipPercentage = when(binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
-
 //        Calculate the tip
         var tip = tipPercentage * cost
-
 //        Round up the tip if required
         val roundUp = binding.roundUpSwitch.isChecked
         if(roundUp){
-            tip = kotlin.math.ceil(tip)
+            tip = ceil(tip)
         }
 
 //        Format the tip
